e3026513498993a7393761232c79d78351939788
Thijs Vandamme <thijs.vandamme@vgtc.be>
2026-09-17 13:59:54 +0200

    Add Passenger WSGI configuration

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "studio.settings"
+)
+
+from studio.wsgi import application
